c966a220186225fdc1836ec1ceebe912a8d34a65
Timorcc <1148137751@qq.com>
2020-12-14 19:31:17 +0800

    1214

---
+++ comlga/src/main/java/JzOffer/No36.java
@@ -0,0 +1,23 @@
+package JzOffer;
+
+import java.util.ArrayList;
+
+/**
+ * @author lga
+ * @date 2020-12-14 16:27
+ * @desc 输入两个链表，找出它们的第一个公共结点。（注意因为传入数据是链表，
+ * 所以错误测试数据的提示是用其他方式显示的，保证传入数据是正确的）
+ */
+public class No36 {
+
+
+
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+}
